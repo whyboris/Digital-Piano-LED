79ae4b2624637c08eacd96eb41ee8eeff01ff58b
Boris Yakubchik <yboris@yahoo.com>
2021-06-08 20:07:44 -0400

    add notes

--- led.py
+++ led.py
@@ -1,4 +1,15 @@
-# to run: `sudo python3 led.py`
+# to run script: `sudo python3 led.py`
+#
+# written by Boris Yakubchik
+#
+# Edit these:
+
+NUM_OF_LED = 144
+BRIGHTNESS = 0.2 # maximum 1 -- best when 1/n is an integer
+PIANO_STARTS_WITH = 'B2'
+
+# =========================================================
+# DO NOT EDIT BELOW
 
 from time import sleep
 from time import time
@@ -12,32 +23,32 @@ import mido
 
 import keyboard
 
-NUM_OF_PIXELS = 144
-
-pixels = neopixel.NeoPixel(board.D18, NUM_OF_PIXELS, brightness=0.2, auto_write=False)
+pixels = neopixel.NeoPixel(board.D18, NUM_OF_LED, brightness=BRIGHTNESS, auto_write=False)
 
 devices = mido.get_input_names()
 piano = []
 
 for device in devices:
-  if device.startswith('B2'):
+  if device.startswith(PIANO_STARTS_WITH):
     piano.append(device)
 
 if len(piano) == 0:
-  print('Piano not connected / turned on?')
+  print('\nIs the piano connected / turned on?\n')
   quit()
 else:
   print('Listening for MIDI from', piano[0])
 
 
 # =======================================================================
-# DECLARE SOME VARIABLES
+# Variables
 
 # array for all BLOBS
 all_blobs = []
 
+min_bright = math.ceil(1 / BRIGHTNESS) 
+
 # array for pixel color computation - to be written to the LED strip later
-rgb = [1] * NUM_OF_PIXELS # fill all of them with 1
+rgb = [1] * NUM_OF_LED # fill all of them with 1
 
 # to throttle keyboard events
 throttle_left = 0 
@@ -67,10 +78,9 @@ def thread_function(name):
 
     global rgb
 
-    # print(len(all_blobs))
+    # print(len(all_blobs)) # to check that blobs disappear after some time
 
-    # first clear out the rgb array
-    rgb = [5] * NUM_OF_PIXELS
+    rgb = [min_bright] * NUM_OF_LED # reset array to min
 
     for blob in all_blobs:
 
@@ -78,21 +88,21 @@ def thread_function(name):
 
       for x in range(math.floor(blob.x - blob.r), math.ceil(blob.x + blob.r)):
 
-        if (x >= 0 and x < NUM_OF_PIXELS):
+        if (x >= 0 and x < NUM_OF_LED):
           scaler = (1 - abs(blob.x - x) / blob.r) ** 2
           rgb[x] = min(rgb[x] + scaler * blob.v, 255) # never let it go above 255
                                            # this can happen because we add many blobs together
 
     for idx, val in enumerate(rgb):
-      pixels[idx] = (max(math.floor(val), 5), 
-                     max(math.floor(val), 5), 
-                     max(math.floor(val), 5))
+      # make sure these are integers
+      pixels[idx] = (math.floor(val), math.floor(val), math.floor(val))
 
     pixels.show()
 
     sleep(0.05)
     
-    all_blobs[:] = [a for a in all_blobs if a.v > 2]
+    # only keep blobs that are above brightness of 1
+    all_blobs[:] = [a for a in all_blobs if a.v > 1]
 
 
 thread = threading.Thread(target=thread_function, args=(1,))
@@ -137,6 +147,8 @@ def throttle_key(key):
 
 # takes raw key (21 - 108) and returns x coordinate
 def map_key_to_x(key):
+  # 65 = center between 21 and 108
+  # 72 = NUM_OF_LEDS / 2 + fudge factor
   return (key - 65) * 2 + 72
 
 
